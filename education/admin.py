9b15aceea3e799fc8a2d51b3ed2e40b972d39171
algrit <algrigorenko91@gmail.com>
2023-04-18 15:29:19 +0300

    course-test-question relations added

--- education/admin.py
+++ education/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from education.models import Question
+from education.models import Question, Test, Course
 
 
 # Register your models here.
@@ -9,5 +9,21 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ['title', 'question_text', 'correct_answer', 'incorrect_answer1', 'incorrect_answer2',
                     'comment']
     fields = ['question_text', 'correct_answer']
+    ordering = ['id']
+    list_per_page = 10
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    filter_horizontal = ['questions']
+    ordering = ['id']
+    list_per_page = 10
+
+
+@admin.register(Course)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    filter_horizontal = ['tests']
     ordering = ['id']
     list_per_page = 10
