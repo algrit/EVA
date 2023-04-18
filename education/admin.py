0ddfda4ca220d3e197603d2c704c2a4aeabc6066
algrit <algrigorenko91@gmail.com>
2023-04-18 12:04:42 +0300

    filling question model

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from education.models import Question
+
+
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'question_text', 'correct_answer', 'incorrect_answer1', 'incorrect_answer2',
+                    'comment']
+    ordering = ['id']
--- education/models.py
+++ education/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=40)
+    question_text = models.TextField()
+    correct_answer = models.TextField()
+    incorrect_answer1 = models.TextField(default='Bad answer 1', blank=True)
+    incorrect_answer2 = models.TextField(default='Bad answer 2', blank=True)
+    comment = models.TextField(default='Your answer is wrong.', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Test(models.Model):
+    title = models.CharField(max_length=40)
+    questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=40)
+    tests = models.ManyToManyField(Test)
+
+    def __str__(self):
+        return self.title
